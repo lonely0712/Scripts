c0e3837b7833c4648320a2b5c3e44b855f7c696c
Adel0726 <adham.elwan.2007@gmail.com>
2025-08-03 13:50:16 +0200

    cd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template, make_response
+from flask import Flask, render_template, send_from_directory, make_response
 
 app = Flask(__name__)
 
@@ -6,13 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/modules/<path:filename>')
 def serve_module(filename):
     response = make_response(send_from_directory('static/modules', filename))
     response.headers['Content-Type'] = 'application/octet-stream'
     response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
     return response
-
-if __name__ == '__main__':
-    app.run(debug=True)
