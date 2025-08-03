20d4ba8035b7e7c111f614a0c97b73a3c9cb4c09
adham <123596633+lonely0712@users.noreply.github.com>
2025-08-03 11:18:06 +0200

    Update Shortform.js

--- Shortform.js
+++ Shortform.js
@@ -1,5 +1,40 @@
 $done({
     body: JSON.stringify({
-        
+        "data" : {
+    "stripe_payment_type" : null,
+    "plan_id" : "plan_annual",
+    "trial_end_datetime" : "2026-08-03T09:09:32.000000+00:00",
+    "is_limited" : false,
+    "trial_start" : 1754212172,
+    "change_plan_at_period_end" : false,
+    "trial_end" : 1785748172,
+    "current_period_amount" : 31536000,
+    "current_period_end" : 1754044736,
+    "billing_plans" : {
+
+    },
+    "trial_start_datetime" : "2025-08-03T09:09:32.000000+00:00",
+    "user_id" : null,
+    "current_period_end_datetime" : "2026-08-03T09:09:32.000000+00:00",
+    "status_message" : "Subscription is active",
+    "cancel_at_period_end" : true,
+    "card_data" : null,
+    "is_trial" : true,
+    "billing_type" : "apple",
+    "status" : "full_access"
+  },
+  "metadata" : {
+    "user" : {
+      "signup" : "2025-07-25T10:36:04.842731+00:00",
+      "id" : null,
+      "is_trial" : true,
+      "email" : null,
+      "language" : null,
+      "temporary_account_state" : null,
+      "is_limited" : false,
+      "plan_id" : "plan_annual",
+      "username" : null
+    }
+  }
     })
 });
