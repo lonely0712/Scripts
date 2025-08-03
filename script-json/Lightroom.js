939a1296e2d1ac223a4e33f4993719b61e6a26f9
Adel0726 <adham.elwan.2007@gmail.com>
2025-08-03 18:52:04 +0200

    eee

--- script-json/Lightroom.js
+++ script-json/Lightroom.js
@@ -4,7 +4,7 @@ obj.entitlement.status="subscriber";
 obj.current_subs={
       "product_id": "lightroom",
       "store": "adobe",
-      "purchase_date": "2019-10-10T16:32:10.254954Z",
+      "purchase_date": "2024-10-10T16:32:10.254954Z",
       "sao": {
         "inpkg_CCES": "0",
         "inpkg_CCLE": "1",
--- script-json/Picsart.js
+++ script-json/Picsart.js
@@ -1,3 +1,5 @@
+
+
 $done({
     body: JSON.stringify({
         "status": "success",
